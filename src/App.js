9678fc897cfb31b51088e329e3a502a239bf8ac0
majazocom <maja.gedda@zocom.se>
2021-03-01 10:40:53 +0100

    json added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
+import { Component } from 'react';
 import './App.css';
+import Child from './components/MyComponent';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Maja är bäst</h1>
-    </div>
-  );
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      counter: 1
+    }
+  }
+  
+  increment = () => {
+    //this.state.counter = this.state.counter + 1;
+    this.setState({counter: this.state.counter + 1})
+  }
+
+  render() {
+    return <section>
+      <h1>HEJ</h1>
+      <h2>{this.state.counter}</h2>
+      <Child handleClick={this.increment} />
+    </section>
+  }
 }
-
 export default App;
