6d7b038c64395a5f0389b2f7347c86ba6eaaa657
majazocom <maja.gedda@zocom.se>
2021-03-01 12:00:31 +0100

    more styling

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,17 +1,46 @@
-import { Component } from 'react';
 import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
 
-class PokemonCard extends Component {
-    render() {
-        let imgUrl = `https://img.pokemondb.net/artwork/${this.props.name}.jpg`;
-        console.log(imgUrl);
-        return (
-            <Card>
-                <h1>{this.props.name}</h1>
-                <img src={imgUrl} alt={this.props.name} width="150" height="150"></img>
-            </Card>
-        )
-    }
+const useStyles = makeStyles({
+    root: {
+        maxWidth: 345,
+    },
+});
+
+function PokemonCard(props) {
+    const classes = useStyles();
+
+    let imgUrl = `https://img.pokemondb.net/artwork/${props.name}.jpg`;
+
+    return (
+        <Card className={classes.root}>
+            <CardActionArea>
+                <CardMedia
+                    component="img"
+                    alt={props.name}
+                    image={imgUrl}
+                    width="150"
+                    title={props.name}
+                />
+                <CardContent>
+                    <Typography
+                        gutterBottom
+                        variant="h3"
+                        component="h2"
+                    >{props.name}</Typography>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                        Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                        across all continents except Antarctica
+          </Typography>
+                </CardContent>
+
+            </CardActionArea>
+        </Card>
+    )
 }
 
 export default PokemonCard;
